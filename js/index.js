c82fbd72b51e37261a93c92accff448fc22e55e1
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-10-04 12:43:18 -0500

    Mobile menu with JS

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const menuButton = document.getElementById('menuButton')
+const closeButton = document.getElementById('closeButton')
+const menuCover = document.getElementById('mobileMenuCover')
+
+function toggleMenu() {
+  menuButton.style.display = 'none';
+  menuCover.style.display = 'block';
+  
+}
+
+function closeMenu() {
+  menuCover.style.display = 'none';
+  menuButton.style.display = 'block';
+
+}
+
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', closeMenu);
