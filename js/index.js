d87e9b72f708856e252f7c77d30bf7e7dd58bd21
codepanther <pezechigozirim@gmail.com>
2021-10-04 20:48:36 +0100

    Style the mobile navigation and blur using javascript

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 const menuButton = document.getElementById('menuButton')
 const closeButton = document.getElementById('closeButton')
 const menuCover = document.getElementById('mobileMenuCover')
+const main = document.querySelector(".main");
 
 function toggleMenu() {
   menuButton.style.display = 'none';
   menuCover.style.display = 'block';
+  main.style.filter = "blur(8px)";
   
 }
 
 function closeMenu() {
   menuCover.style.display = 'none';
   menuButton.style.display = 'block';
+  main.style.filter = "blur(0px)"
+
 }
 
 menuButton.addEventListener('click', toggleMenu);
