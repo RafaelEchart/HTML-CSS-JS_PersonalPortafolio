f3e331e722a0bc037752a1a74c62e0cee8c6a263
codepanther <pezechigozirim@gmail.com>
2021-10-06 17:16:35 +0100

    Style the error message and dynamically show errors

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,21 +2,19 @@
 /* This function is being called from the HTML */
 function checkForm(form) {
   const email = document.getElementById('email');
-  console.log(email);
+  const error = document.getElementById('errorMessage');
+
   if (email.value === email.value.toLowerCase()) {
-    console.log(true);
     return true;
   } else {
 
-    //email
-    //border - red
-    //shadow - red
-    //innerHTML - error
-    //Email must be lowercase!
-    //display - block - error
+    email.style.border = '1px solid red';
+    email.style.boxShadow = '0 0 5pt 1pt #ff0000';
+
+    error.innerHTML = "Email must be lowercase!";
+    error.style.display = "block";
 
     form.email.focus();
     return false;
-
   }
 }
